52a072b7c8dadc4c803613f962465b9087497d09
Tender <142536276+Tnders@users.noreply.github.com>
2024-12-01 15:18:28 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ util.services.players = game:GetService("Players")
 function util.functions:getPlayers()
     local players = {}
     for _, player in pairs(util.services.players:GetPlayers()) do
-        if player.Character and player.Character.PrimaryPart then
+        if player ~= util.services.players.LocalPlayer and player.Character and player.Character.PrimaryPart then
             table.insert(players, player)
         end
     end
